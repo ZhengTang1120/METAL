bf3e04d6274dfcaf76979c6fa0902be61ca28ec5
Zheng Tang <oscartang1120@gmail.com>
2022-06-07 18:14:04 -0700

    Update train.py

--- train.py
+++ train.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
     if args.train:
         config = ConfigFactory.parse_file(args.config)
         taskManager = TaskManager(config, 1234)
-        for task in taskManager:
+        for task in taskManager.tasks:
             train_df = read_sents(taskManager.trainSentences)
 
             print (train_df)
