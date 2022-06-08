38f614ed5bf662af1a44237b31746bc1fc24356c
Zheng Tang <oscartang1120@gmail.com>
2022-06-07 18:10:41 -0700

    Update train.py

--- train.py
+++ train.py
@@ -2,6 +2,21 @@ from pyhocon import ConfigFactory
 import argparse
 from taskManager import TaskManager
 
+import numpy as np
+import pandas as pd
+
+def read_sents(sentences):
+    data = {'words': [], 'ners': []}
+    for sent in sentences:
+        words = list()
+        ners = list()
+        for row in sent:
+            words.append(row.tokens[0])
+            ners.append(row.tokens[1])
+        data['words'].append(words)
+        data['ners'].append(ners)
+    return pd.DataFrame(data)
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
@@ -14,8 +29,6 @@ if __name__ == '__main__':
         config = ConfigFactory.parse_file(args.config)
         taskManager = TaskManager(config, 1234)
 
-        for i, t in enumerate(taskManager.tasks):
-            print (i)
-            for sent in t.trainSentences:
-                for row in sent:
-                    print (row.tokens)
+        train_df = read_sents(taskManager[0].trainSentences)
+
+        print (train_df)
