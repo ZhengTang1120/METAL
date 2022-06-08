e784a2aa9566b535ac001eeb29f140e4928431a8
Zheng Tang <oscartang1120@gmail.com>
2022-06-08 11:33:27 -0700

    Update layers.py

--- layers.py
+++ layers.py
@@ -164,6 +164,8 @@ class RnnLayer(IntermediateLayer):
     def forward(self, input_states, lengths):
         packed = pack_padded_sequence(input_states, lengths, batch_first=True, enforce_sorted=False)
         states, _ = self.rnn(packed)
+        states, _ = pad_packed_sequence(states, batch_first=True)
+        print (states.size(), input_states.size())
         if self.highway_connection:
             states =  torch.cat([states, input_states], dim=1)
         return self.dropout(states)
