89a3a764c25b7bb2cf577805be9a129dabe32150
Zheng Tang <oscartang1120@gmail.com>
2022-06-08 01:28:36 -0700

    ..

--- train.py
+++ train.py
@@ -35,6 +35,30 @@ def read_sents(sentences):
 def get_ids(tokens, key_to_index, unk_id=None):
     return [key_to_index.get(tok, unk_id) for tok in tokens]
 
+class MyDataset(Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def __len__(self):
+        return len(self.y)
+    
+    def __getitem__(self, index):
+        x = torch.tensor(self.x[index])
+        y = torch.tensor(self.y[index])
+        return x, y
+
+def collate_fn(batch):
+    # separate xs and ys
+    xs, ys = zip(*batch)
+    # get lengths
+    lengths = [len(x) for x in xs]
+    # pad sequences
+    x_padded = pad_sequence(xs, batch_first=True, padding_value=pad_tok_id)
+    y_padded = pad_sequence(ys, batch_first=True, padding_value=pad_tag_id)
+    # return padded
+    return x_padded, y_padded, lengths
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
