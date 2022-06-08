3fcdd2c8074f6dd479b89296301625f492f42a94
Zheng Tang <oscartang1120@gmail.com>
2022-06-07 18:46:27 -0700

    Update train.py

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from taskManager import TaskManager
 
 import numpy as np
 import pandas as pd
-from tqdm.notebook import tqdm
+from tqdm import tqdm
 tqdm.pandas()
 
 from gensim.models import KeyedVectors
