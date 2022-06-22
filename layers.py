6c3c8acfbfad3ba9b78482c9ad6a5819c3297d94
Zheng Tang <oscartang1120@gmail.com>
2022-06-21 23:04:02 -0700

    use bert to replace embedding layer and rnn layer

--- layers.py
+++ layers.py
@@ -2,6 +2,8 @@ import torch
 from torch import nn
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
+from transformers import BertModel, BertConfig
+
 DEFAULT_DROPOUT_PROB: float = 0.1
 DEFAULT_LEARNED_WORD_EMBEDDING_SIZE: int = 128
 DEFAULT_CHAR_EMBEDDING_SIZE: int = 32
@@ -21,36 +23,40 @@ class Layers(nn.Module):
 
         paramPrefix = "mtl.layers"
 
-        learnedWordEmbeddingSize = config.get_int(paramPrefix + ".initial" + ".learnedWordEmbeddingSize",DEFAULT_LEARNED_WORD_EMBEDDING_SIZE)
-        # charEmbeddingSize        = config.get_int(paramPrefix + ".charEmbeddingSize",DEFAULT_CHAR_EMBEDDING_SIZE)
-        # charRnnStateSize         = config.get_int(paramPrefix + ".charRnnStateSize",DEFAULT_CHAR_RNN_STATE_SIZE)
-        posTagEmbeddingSize      = config.get_int(paramPrefix + ".initial" + ".posTagEmbeddingSize",DEFAULT_POS_TAG_EMBEDDING_SIZE)
-        neTagEmbeddingSize       = config.get_int(paramPrefix + ".initial" + ".neTagEmbeddingSize",DEFAULT_NE_TAG_EMBEDDING_SIZE)
-        distanceEmbeddingSize    = config.get_int(paramPrefix + ".initial" + ".distanceEmbeddingSize",DEFAULT_DISTANCE_EMBEDDING_SIZE)
-        distanceWindowSize       = config.get_int(paramPrefix + ".initial" + ".distanceWindowSize",DEFAULT_DISTANCE_WINDOW_SIZE)
-        useIsPredicate           = config.get_bool(paramPrefix + ".initial" + ".useIsPredicate",DEFAULT_USE_IS_PREDICATE==1)
-        positionEmbeddingSize    = config.get_int(paramPrefix + ".initial" + ".positionEmbeddingSize",DEFAULT_POSITION_EMBEDDING_SIZE)
-        dropoutProb              = config.get_float(paramPrefix + ".initial" + ".dropoutProb",DEFAULT_DROPOUT_PROB)
-        predicateDim             = 1 if distanceEmbeddingSize and useIsPredicate else 0
+        # learnedWordEmbeddingSize = config.get_int(paramPrefix + ".initial" + ".learnedWordEmbeddingSize",DEFAULT_LEARNED_WORD_EMBEDDING_SIZE)
+        # # charEmbeddingSize        = config.get_int(paramPrefix + ".charEmbeddingSize",DEFAULT_CHAR_EMBEDDING_SIZE)
+        # # charRnnStateSize         = config.get_int(paramPrefix + ".charRnnStateSize",DEFAULT_CHAR_RNN_STATE_SIZE)
+        # posTagEmbeddingSize      = config.get_int(paramPrefix + ".initial" + ".posTagEmbeddingSize",DEFAULT_POS_TAG_EMBEDDING_SIZE)
+        # neTagEmbeddingSize       = config.get_int(paramPrefix + ".initial" + ".neTagEmbeddingSize",DEFAULT_NE_TAG_EMBEDDING_SIZE)
+        # distanceEmbeddingSize    = config.get_int(paramPrefix + ".initial" + ".distanceEmbeddingSize",DEFAULT_DISTANCE_EMBEDDING_SIZE)
+        # distanceWindowSize       = config.get_int(paramPrefix + ".initial" + ".distanceWindowSize",DEFAULT_DISTANCE_WINDOW_SIZE)
+        # useIsPredicate           = config.get_bool(paramPrefix + ".initial" + ".useIsPredicate",DEFAULT_USE_IS_PREDICATE==1)
+        # positionEmbeddingSize    = config.get_int(paramPrefix + ".initial" + ".positionEmbeddingSize",DEFAULT_POSITION_EMBEDDING_SIZE)
+        # dropoutProb              = config.get_float(paramPrefix + ".initial" + ".dropoutProb",DEFAULT_DROPOUT_PROB)
+        # predicateDim             = 1 if distanceEmbeddingSize and useIsPredicate else 0
 
-        self.initialLayer = EmbeddingLayer(embeddings, word_size, learnedWordEmbeddingSize, dropoutProb, 
-                            postag_size, posTagEmbeddingSize, ner_size, neTagEmbeddingSize, 
-                            distanceWindowSize, distanceEmbeddingSize, positionEmbeddingSize, 
-                            useIsPredicate)
-        input_size = embeddings.shape[1] + learnedWordEmbeddingSize + posTagEmbeddingSize + neTagEmbeddingSize + distanceEmbeddingSize + positionEmbeddingSize + predicateDim
+        # self.initialLayer = EmbeddingLayer(embeddings, word_size, learnedWordEmbeddingSize, dropoutProb, 
+        #                     postag_size, posTagEmbeddingSize, ner_size, neTagEmbeddingSize, 
+        #                     distanceWindowSize, distanceEmbeddingSize, positionEmbeddingSize, 
+        #                     useIsPredicate)
+        # input_size = embeddings.shape[1] + learnedWordEmbeddingSize + posTagEmbeddingSize + neTagEmbeddingSize + distanceEmbeddingSize + positionEmbeddingSize + predicateDim
         # Work for the 1 intermediate layer 1 final layer scenario for now.
 
-        self.intermediateLayers = list()
-        i = 1
-        numLayers = config.get_int(paramPrefix + f".intermediate{i}" + ".numLayers", 1)
-        rnnStateSize = config.get_int(paramPrefix + f".intermediate{i}" + ".rnnStateSize", None)
-        useHighwayConnections = config.get_bool(paramPrefix + f".intermediate{i}" + '.useHighwayConnections', False)
-        rnnType = config.get_string(paramPrefix + f".intermediate{i}" + ".type", "lstm")
-        dropoutProb = config.get_float(paramPrefix + f".intermediate{i}" + ".dropoutProb", DEFAULT_DROPOUT_PROB)
-        intermediateLayer = RnnLayer(input_size, numLayers, rnnStateSize, rnnType, dropoutProb, useHighwayConnections)
-        self.intermediateLayers.append(intermediateLayer)
-        highwaySize = input_size if useHighwayConnections else 0
-        input_size = 2 * rnnStateSize + highwaySize
+        self.initialLayer = BERTLayer("bert-base-uncased")
+        config = BertConfig.from_pretrained(opt['bert'])
+        input_size = config.hidden_size
+
+        # self.intermediateLayers = list()
+        # i = 1
+        # numLayers = config.get_int(paramPrefix + f".intermediate{i}" + ".numLayers", 1)
+        # rnnStateSize = config.get_int(paramPrefix + f".intermediate{i}" + ".rnnStateSize", None)
+        # useHighwayConnections = config.get_bool(paramPrefix + f".intermediate{i}" + '.useHighwayConnections', False)
+        # rnnType = config.get_string(paramPrefix + f".intermediate{i}" + ".type", "lstm")
+        # dropoutProb = config.get_float(paramPrefix + f".intermediate{i}" + ".dropoutProb", DEFAULT_DROPOUT_PROB)
+        # intermediateLayer = RnnLayer(input_size, numLayers, rnnStateSize, rnnType, dropoutProb, useHighwayConnections)
+        # self.intermediateLayers.append(intermediateLayer)
+        # highwaySize = input_size if useHighwayConnections else 0
+        # input_size = 2 * rnnStateSize + highwaySize
 
         i = 1
         nonlinAsString = config.get_string(f"mtl.task{i}.layers" + ".final" + ".nonlinearity", "")
@@ -64,8 +70,8 @@ class Layers(nn.Module):
 
         if self.initialLayer is not None:
             states = self.initialLayer(states)
-        for intermediateLayer in self.intermediateLayers:
-            states = intermediateLayer(states, lengths)
+        # for intermediateLayer in self.intermediateLayers:
+        #     states = intermediateLayer(states, lengths)
         if self.finalLayer is not None:
             states = self.finalLayer(states, headPositions)
 
@@ -90,6 +96,18 @@ class FinalLayer(nn.Module):
         super().__init__()
     def forward(self, inputExpressions, modifierHeadPairs):
         raise NotImplementedError
+
+class BERTLayer(InitialLayer):
+    def __init__(self, model_name):
+        super().__init__()
+        self.model = BertModel.from_pretrained(model_name)
+
+    def forward(self, words, segment_ids=None, masks=None):
+        output = self.model(words)
+        h = output.last_hidden_state
+
+        return h
+
 
 class EmbeddingLayer(InitialLayer):
     def __init__(self, embeddings, word_size, learnedWordEmbeddingSize, dropout, 
