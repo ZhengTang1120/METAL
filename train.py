495a839e71a66f6336b84d00b6260a4dddaa6d62
Zheng Tang <oscartang1120@gmail.com>
2022-06-28 13:52:11 -0700

    Update train.py

--- train.py
+++ train.py
@@ -11,7 +11,7 @@ from gensim.models import KeyedVectors
 
 from torch import optim
 from torch.utils.data import DataLoader
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 from dataloader import *
 
@@ -114,6 +114,7 @@ num_layers = 2
 bidirectional = True
 dropout = 0.1
 output_size = len(index_to_ner)
+print (index_to_ner)
 word_size = vectors.shape[0]
 
 # initialize the model, loss function, optimizer, and data-loader
@@ -164,6 +165,8 @@ for epoch in range(n_epochs):
     model.eval()
     with torch.no_grad():
         losses, acc = [], []
+        golds = []
+        preds = []
         for x_padded, y_padded, lengths in tqdm(dev_dl, desc=f'epoch {epoch+1} (dev)'):
             x_padded = x_padded
             y_padded = y_padded
@@ -174,14 +177,15 @@ for epoch in range(n_epochs):
             y_true = y_true[mask]
             y_pred = y_pred[mask]
             loss = loss_func(y_pred, y_true)
-            gold = y_true.cpu().numpy()
-            pred = np.argmax(y_pred.cpu().numpy(), axis=1)
+            gold = y_true.cpu().numpy().tolist()
+            pred = np.argmax(y_pred.cpu().numpy(), axis=1).tolist()
             losses.append(loss.cpu().item())
+            golds += gold
+            preds += pred
             acc.append(accuracy_score(gold, pred))
         dev_loss.append(np.mean(losses))
         dev_acc.append(np.mean(acc))
-
-        print (dev_loss[-1], dev_acc[-1])
+        print (dev_loss[-1], dev_acc[-1], f1_score(np.array(golds), np.array(preds), labels=[l for l in index_to_ner if l!='O' and l!='<pad>'], average='micro'))
 
 
 
